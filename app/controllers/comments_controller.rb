eb1138a8eb32700b9b93f8c6001ec0571b756aa9
giangmt <maitruonggiangbkhn@gmail.com>
2018-07-08 15:56:51 +0700

    Filter and Indifferent Access

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,22 @@
 class CommentsController < ApplicationController
+  before_action :find_post, only: [:create, :destroy]
   def create
-    @post     = Post.find(params['post_id'])
-    @comment  = post.build_comment('body' => params['body'], 'author' => params['author'])
+    @comment  = post.build_comment(body: params[:body], author: params[:author])
     if @comment.save
-      redirect_to posts_path(@posts.id)
+      redirect_to posts_path(@post.id)
     else
       render 'posts/show',
     end
   end
 
   def destroy
-    post = Post.find params[:post_id]
-    post.delete_comment params[:comment_id]
-    redirect_to posts_path(@posts.id)
+    @post.delete_comment params[:comment_id]
+    redirect_to posts_path(@post.id)
+  end
+
+  private
+
+  def find_post
+    @post = Post.find params[:post_id]
   end
 end
