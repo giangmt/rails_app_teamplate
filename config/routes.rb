d691a63ee256ddc3b2d2fe95e1e255c8c5b2fb14
giangmt <maitruonggiangbkhn@gmail.com>
2018-07-08 10:57:20 +0700

    RCC Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,15 @@
 Rails.application.routes.draw do
   get '/hello_world', to: 'application#hello_world'
 
-  get '/list_posts', to: 'application#list_posts'
+  get '/posts', to: 'posts#index'
+  get '/posts/:id', to: 'posts#show'
+  get 'posts/new', to: 'posts#new'
+  post 'posts', to: 'posts#create'
+  get 'posts/:id/edit', to: 'posts#edit'
+  patch 'posts/:id', to: 'posts#update'
+  put 'posts/:id', to: 'posts#update'
+  delete 'posts/:id', to: 'posts#destroy'
 
-  get '/show_post/:id', to: 'application#show_post'
-
-  get '/new_post', to: 'application#new_post'
-  post '/create_post', to: 'application#create_post'
-
-  get '/edit_post/:id', to: 'application#edit_post'
-  post '/update_post/:id', to: 'application#update_post'
-
-  post '/delete_post/:id', to: 'application#delete_post'
-
-  post '/create_comment_for_post/:post_id' => 'application#create_comment'
-  
-  post '/list_posts/:post_id/delete_comment/:comment_id' => 'application#delete_comment'
+  post 'posts/:id/comments', to: 'comments#create'
+  delete 'posts/:post_id/comments/:id', to: 'comments#destroy'
 end
