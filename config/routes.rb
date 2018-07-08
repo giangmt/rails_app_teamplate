46f110242f36f682311c47ccc58d9bb4aa46fa25
giangmt <maitruonggiangbkhn@gmail.com>
2018-07-08 11:37:43 +0700

    RCC Resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
+  root to: 'posts#index'
   get '/hello_world', to: 'application#hello_world'
 
-  get '/posts', to: 'posts#index'
-  get '/posts/:id', to: 'posts#show'
-  get 'posts/new', to: 'posts#new'
-  post 'posts', to: 'posts#create'
-  get 'posts/:id/edit', to: 'posts#edit'
-  patch 'posts/:id', to: 'posts#update'
-  put 'posts/:id', to: 'posts#update'
-  delete 'posts/:id', to: 'posts#destroy'
+  resources :posts do
+    collection do
+      get 'popular', action: :index, popular: true
+    end
 
-  post 'posts/:id/comments', to: 'comments#create'
-  delete 'posts/:post_id/comments/:id', to: 'comments#destroy'
+    get 'preview', on: :member
+    resources :comments, only: [:create, :destroy]
+  end
+
+  match 'author/:id', to: 'authors#update', via: [:put, :patch], as: :authors_update
+
+  get '/home', to: redirect('/')
 end
